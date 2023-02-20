e7213022e2a74878274260ea12212ff61f739492
Sofia Acevedo <sofiacandelaria1992@gmail.com>
2023-02-20 23:08:42 +0000

    configurar slider

---
+++ js/mi-jquery.js
@@ -0,0 +1,16 @@
+// cuando el documento este listo 
+$(document).ready(function(){
+
+
+// codigo para el slider copiado desde bxslider 
+$(function(){
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      captions: false,
+      slideWidth: 1200,
+      slideHeigth: 300,
+    });
+  });
+
+//cierre del documente ready
+})
